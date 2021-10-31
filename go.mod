5d9cd35b2e79efc40e7707efeaa6f65e1198c4a6
infinitebash <32340104+infinitebash@users.noreply.github.com>
2021-10-30 23:09:37 -0700

    TODO: bootstrap pax
    Co-authored-by: lleyton <lleyton@innatical.com>

--- go.mod
+++ go.mod
@@ -3,3 +3,21 @@ module github.com/innatical/pax-chroot
 go 1.17
 
 require golang.org/x/sys v0.0.0-20211030160813-b3129d9d1021
+
+require (
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/Masterminds/semver v1.5.0 // indirect
+	github.com/charmbracelet/lipgloss v0.4.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/innatical/apkg/v2 v2.0.11 // indirect
+	github.com/innatical/pax/v2 v2.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.9.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/ulikunitz/xz v0.5.10 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,25 +2,110 @@ package main
 
 import (
 	"io/ioutil"
+	"os"
+	"path/filepath"
+	"syscall"
 
+	"github.com/innatical/pax/v2/util"
 	"golang.org/x/sys/unix"
 )
 
 func main() {
+	name, err := ioutil.TempDir("/tmp", "pax-chroot")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := SetupChroot(name); err != nil {
+		panic(err)
+	}
+
+	exit, err := OpenChroot(name)
+	if err != nil {
+		panic(err)
+	}
+
+	util.Install(name, "linux", "5.13", true)
+
+	if err := exit(); err != nil {
+		panic(err)
+	}
+
+	// if err := CleanupChroot(name); err != nil {
+	// 	panic(err)
+	// }
+}
+
+func OpenChroot(name string) (func() error, error) {
+	root, err := os.Open("/")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := os.Chdir(name); err != nil {
+		return nil, err
+	}
+
+	if err := syscall.Chroot(name); err != nil {
+		root.Close()
+		return nil, err
+	}
+
+	exit := func() error {
+		defer root.Close()
+		if err := root.Chdir(); err != nil {
+			return err
+		}
+		return syscall.Chroot(".")
+	}
+
+	return exit, nil
 
 }
 
-func setup_chroot() error {
-	name, err := ioutil.TempDir("/tmp", "pax-chroot")
-	if err != nil {
+func SetupChroot(name string) error {
+	procdir := filepath.Join(name, "proc")
+	if err := os.Mkdir(procdir, 0777); err != nil {
+		return err
+	}
+	if err := unix.Mount("/proc", procdir, "proc", 0, "rw"); err != nil {
 		return err
 	}
 
-	if err := unix.Mount("/proc", name+"/proc", "proc"); err != nil {
+	sysdir := filepath.Join(name, "sys")
+	if err := os.Mkdir(sysdir, 0777); err != nil {
+		return err
+	}
+	if err := unix.Mount("/sys", sysdir, "sysfs", 0, "rw"); err != nil {
 		return err
 	}
 
-	if err := unix.Mount("/sys", name+"/sys", "sysfs"); err != nil {
+	devdir := filepath.Join(name, "dev")
+	if err := os.Mkdir(devdir, 0777); err != nil {
 		return err
 	}
+	if err := unix.Mount("/dev", devdir, "none", unix.MS_BIND, ""); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func CleanupChroot(name string) error {
+	procdir := filepath.Join(name, "proc")
+	if err := unix.Unmount(procdir, 0); err != nil {
+		return err
+	}
+
+	sysdir := filepath.Join(name, "sys")
+	if err := unix.Unmount(sysdir, 0); err != nil {
+		return err
+	}
+
+	devdir := filepath.Join(name, "dev")
+	if err := unix.Unmount(devdir, 0); err != nil {
+		return err
+	}
+
+	return nil
 }
